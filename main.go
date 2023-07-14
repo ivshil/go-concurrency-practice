f73dbff632de9557028f69781b94bf42861ea089
ivshil-lklt <ivanshild@hotmail.com>
2023-07-14 11:55:52 +0300

    Basic tasks done

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	//TODO
+	go printMessage("Hello, World!")
+	fmt.Println("Goroutine started!")
+	time.Sleep(1 * time.Second)
+
+	fileNames := []string{"Readme.md", "main.go", "go.mod", "main_test.go", "process_tracker.go"}
+	for i := 0; i < len(fileNames); i++ {
+		go processFile(fileNames[i])
+	}
+
+	fmt.Println("Well, well, well..")
+	time.Sleep(6 * time.Second)
+	fmt.Println("End of Program")
+}
+
+func printMessage(str string) {
+	fmt.Println(str)
+}
+
+func processFile(fileName string) {
+	time.Sleep(3 * time.Second)
+	fmt.Printf("\nProcessing is being done with file: %s\n", fileName)
+	time.Sleep(2 * time.Second)
 }
