82652cb99fd9e2cc4fee1acd1361da8c3519a0f2
ivshil-lklt <ivanshild@hotmail.com>
2023-07-19 14:03:48 +0300

    Updated the functionality to work with channels, sync.WaitGroup & defers, also added notes

--- main.go
+++ main.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	go printMessage("Hello, World!")
 	fmt.Println("Goroutine started!")
-	time.Sleep(1 * time.Second)
+
+	c := make(chan string)
+	var wg sync.WaitGroup
 
 	fileNames := []string{"Readme.md", "main.go", "go.mod", "main_test.go", "process_tracker.go"}
-	for i := 0; i < len(fileNames); i++ {
-		go processFile(fileNames[i])
+	fileNamesLen := len(fileNames)
+
+	wg.Add(fileNamesLen)
+
+	for i := 0; i < fileNamesLen; i++ {
+		go processFile(fileNames[i], c, &wg)
+	}
+
+	go func() {
+		wg.Wait() // Wait for all goroutines to complete.
+		close(c)
+	}()
+
+	for msg := range c {
+		fmt.Println(msg)
 	}
 
 	fmt.Println("Well, well, well..")
-	time.Sleep(6 * time.Second)
 	fmt.Println("End of Program")
 }
 
@@ -24,8 +39,11 @@ func printMessage(str string) {
 	fmt.Println(str)
 }
 
-func processFile(fileName string) {
-	time.Sleep(3 * time.Second)
-	fmt.Printf("\nProcessing is being done with file: %s\n", fileName)
-	time.Sleep(2 * time.Second)
+func processFile(fileName string, c chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Simulate some processing time
+	time.Sleep(12 * time.Millisecond)
+
+	c <- fmt.Sprintf("\nProcessing is being done with file: %s\n", fileName)
 }
